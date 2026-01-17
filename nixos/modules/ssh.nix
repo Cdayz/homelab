19c42898abb8b7d70f6a0c13ac65dbb4382f1adf
cdayz <capitan.crazy.dayz@gmail.com>
2026-01-17 16:54:20 +0300

    Improve ssh hardening

--- nixos/modules/ssh.nix
+++ nixos/modules/ssh.nix
@@ -7,10 +7,18 @@
       PasswordAuthentication = false;
       KbdInteractiveAuthentication = false;
       ChallengeResponseAuthentication = false;
+
       X11Forwarding = false;
       AllowTcpForwarding = "yes";
       AllowAgentForwarding = "no";
       UseDns = false;
+
+      MaxAuthTries = 3;
+      LoginGraceTime = "30s";
     };
+
+    extraConfig = ''
+      AllowUsers crazy
+    '';
   };
 }
