b2002e2a68497bccb14ba4166bd8c552265c9cef
cdayz <capitan.crazy.dayz@gmail.com>
2026-01-17 16:59:49 +0300

    Fix power config

--- nixos/modules/power.nix
+++ nixos/modules/power.nix
@@ -9,13 +9,13 @@
     powerKey = "ignore";
     powerKeyLongPress = "ignore";
 
-    extraConfig = ''
-      IdleAction=ignore
-      IdleActionSec=0
-    '';
+    settings.Login = {
+      IdleAction = "ignore";
+      IdleActionSec = 0;
+    };
   };
 
-  # На всякий случай отключаем sleep targets
+  # Полностью отключаем sleep/suspend/hibernate targets
   systemd.targets.sleep.enable = false;
   systemd.targets.suspend.enable = false;
   systemd.targets.hibernate.enable = false;
