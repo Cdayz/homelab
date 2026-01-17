77040740ae71e91b5692dca2cd2b415e4313f540
cdayz <capitan.crazy.dayz@gmail.com>
2026-01-17 16:49:04 +0300

    Fix fail2ban #3

--- nixos/modules/fail2ban.nix
+++ nixos/modules/fail2ban.nix
@@ -3,17 +3,27 @@
 {
   services.fail2ban = {
     enable = true;
+    maxretry = 5;
+    ignoreIP = [
+      "192.168.0.0/16"
+    ];
 
-    extraSettings = {
-      bantime = "1h";
-      findtime = "10m";
-      maxretry = 5;
+    bantime = "24h";
+    bantime-increment = {
+      enable = true;
+      formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
+      multipliers = "1 2 4 8 16 32 64";
+      maxtime = "168h";
+      overalljails = true;
     };
 
     jails = {
       sshd = {
         enabled = true;
         backend = "systemd";
+        settings = {
+          mode = "normal";
+        };
       };
     };
   };
