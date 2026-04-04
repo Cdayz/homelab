3bd64ca5d5f47472e4285b1b1a335fcb85bc9a61
cdayz <capitan.crazy.dayz@gmail.com>
2026-04-04 22:21:35 +0300

    cleanup repo

--- nixos/modules/system/directories.nix
+++
@@ -1,7 +0,0 @@
-{
-  systemd.tmpfiles.rules = [
-    "d /var/lib/nomad 0755 nomad nomad -"
-    "d /var/lib/nomad/alloc 0755 nomad nomad -"
-    "d /var/lib/nomad/alloc_mounts 0755 nomad nomad -"
-  ];
-}
