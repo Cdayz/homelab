0cd39475049cb0104cbf6e0f75b535e65cfd1b9e
cdayz <capitan.crazy.dayz@gmail.com>
2026-04-11 23:27:01 +0300

    basic system setup

--- nixos/modules/base/default.nix
+++ nixos/modules/base/default.nix
@@ -7,5 +7,10 @@
     ./timezone.nix
     ./sudo.nix
     ./packages.nix
+    ./journald.nix
+    ./cleaning.nix
+    ./bootloader.nix
+    ./power.nix
+    ./docker.nix
   ];
 }
--- nixos/modules/base/packages.nix
+++ nixos/modules/base/packages.nix
@@ -4,7 +4,13 @@
   environment.systemPackages = with pkgs; [
     git
     vim
+    htop
+    tmux
+    curl
     wget
+    age
+    sops
+    go-task
     fish
   ];
   programs.fish.enable = true;
