591ac5674e42f566f558acdf52be9b02df0c81da
cdayz <capitan.crazy.dayz@gmail.com>
2026-05-11 20:23:53 +0300

    Enabled zfs snapshots

--- nixos/modules/storage/zfs.nix
+++ nixos/modules/storage/zfs.nix
@@ -12,4 +12,13 @@
 
   services.zfs.autoScrub.enable = true;
   services.zfs.trim.enable = true;
+  services.zfs.autoSnapshot = {
+    enable = true;
+
+    frequent = 4;
+    hourly = 24;
+    daily = 7;
+    weekly = 4;
+    monthly = 3;
+  };
 }
