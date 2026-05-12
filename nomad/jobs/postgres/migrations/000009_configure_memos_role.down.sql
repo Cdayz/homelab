731e2ca60a057014317d16d40a98c4224627540a
cdayz <capitan.crazy.dayz@gmail.com>
2026-05-12 11:23:58 +0300

    Remove memos

--- nomad/jobs/postgres/migrations/000009_configure_memos_role.down.sql
+++
@@ -1,8 +0,0 @@
-DO $$
-BEGIN
-    IF EXISTS (
-        SELECT FROM pg_user WHERE usename = 'memos'
-    ) THEN
-        DROP USER memos;
-    END IF;
-END $$;
--- nomad/jobs/postgres/migrations/000010_configure_memos_db.down.sql
+++
@@ -1,9 +0,0 @@
-DO $$
-BEGIN
-    IF EXISTS (
-        SELECT 1 FROM pg_database WHERE datname = 'memos'
-    ) THEN
-        PERFORM dblink_exec('dbname=' || current_database()  -- current db
-                        , 'DROP DATABASE memos;');
-    END IF;
-END $$;
